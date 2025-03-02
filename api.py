4083fc29932934587e935a066a7ac42332ee4e54
PPure123 <123245253+PPure123@users.noreply.github.com>
2025-03-02 22:08:21 +0700

    Update api.py

--- api.py
+++ api.py
@@ -69,4 +69,4 @@ def predict():
         return render_template('index.html', prediction=f"เกิดข้อผิดพลาด: {str(e)}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
